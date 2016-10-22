f70621bdd6f89b9abde8b47af6ed13ac1dfb669d
gorwang <danny.roux@hotmail.fr>
2016-10-21 20:47:10 -0400

    16th commit
    started the my part of the project

---
+++ Project/TeacherActivity/Teacher/app/src/main/java/com/danny/teacher/MonitorActivityFragment.java
@@ -0,0 +1,22 @@
+package com.danny.teacher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MonitorActivityFragment extends Fragment {
+
+    public MonitorActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
---
+++ Project/TeacherActivity/Teacher/app/src/main/java/com/danny/teacher/SetAssignsActivityFragment.java
@@ -0,0 +1,22 @@
+package com.danny.teacher;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SetAssignsActivityFragment extends Fragment {
+
+    public SetAssignsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
